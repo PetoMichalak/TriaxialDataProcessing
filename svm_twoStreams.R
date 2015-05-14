71f7dbffe22d5a521b3a03be642667a4ea7f4129
Peter Michalák <peto.michalak@gmail.com>
2015-05-14 10:17:47 +0100

    svm stream quality works!

--- svm_twoStreams.R
+++ svm_twoStreams.R
@@ -1,4 +1,6 @@
 # svm for two streams of triaxial data
+require(e1071)
+require(tools)
 
 # === modify to suit your needs
 # path to a case study
@@ -10,16 +12,18 @@ wristTrainPath = "TrainData/wrist"
 hipTrainPath = "TrainData/hip"
 # list of booleans to specify number of features to work with
 # add up to 234
-fftCount = 1
+fftCount = 15
 # ===
 
 # log file
 logPath = paste(path, "/partialResult_svm.log", sep="")
-write("filename,source,SVM",file=logPath, append=FALSE)
+write("filename,source,cost,gamma",file=logPath, append=FALSE)
 
 # setup the filter
-filterTestData = c(rep(TRUE, 9), rep(c(rep(TRUE, fftCount), rep(FALSE, 15-fftCount)), 15))
-filterTrainData = c(rep(TRUE, 9), rep(c(rep(TRUE, fftCount), rep(FALSE, 15-fftCount)), 15))
+filterTestData = c(rep(TRUE, 9),
+                   rep(c(rep(TRUE, fftCount), rep(FALSE, 15-fftCount)), 15))
+filterTrainData = c(rep(TRUE, 9),
+                    rep(c(rep(TRUE, fftCount), rep(FALSE, 15-fftCount)), 15))
 
 # load project specific libraries
 source("activityRecognitionFunctions.R")
@@ -46,27 +50,114 @@ testHip = testHip[,filterTest]
 trainWrist = trainWrist[,filterTrain]
 trainHip = trainHip[,filterTrain]
 
-# run kNN
-testWristSQ = rep(0, length(kNN_classifiers))
-testHipSQ = rep(0, length(kNN_classifiers))
-# drop timestamp and activity
-drops = c("timestamp", "activity")
+###########
+# run SVM #
+###########
 
-tuned <- tune.svm(activity~., data = trainHip[,!(names(trainHip) %in% c("timestamp"))], 
-                  gamma = 10^(-6:-1), cost = 10^(-1:1)) 
-model  <- svm(activity~., data = trainHip[,!(names(trainHip) %in% c("timestamp"))], 
-              gamma=tuned$best.parameters[1,1], 
-              cost=tuned$best.parameters[1,2])
-prediction <- predict(model, testHip[,!(names(testHip) %in% drops)])
-tab <- table(pred = prediction, true = testHip[,"activity"])
-tab
+# Stream Quality placeholders
+testWristSQ = NA
+testHipSQ = NA
 
-trainDF$activity = factor(trainDF$activity)
-svm.model <- svm(activity ~ ., data = trainDF, cost = 100, gamma = 1, probability = TRUE)
-svm.pred <- predict(svm.model, testDF["statSummary"], probability = TRUE)
-svm.pred
+# Wrist stream
+trainWrist$activity = factor(trainWrist$activity)
 
-svm_SQ = rep(NA, length(svm.pred))
-for (i in 1:length(svm.pred)) {
-  svm_SQ[i] = attributes(svm.pred)$prob[i,as.character(svm.pred[i])]
+# find best cost and gamma parameters for SVM
+tuned.wrist <- tune.svm(activity ~ ., data = trainWrist[,-1],
+                        gamma = 10^(-6:-1), cost = 10^(-1:1)) 
+
+# train model (ignoring timestamp)
+svm.model.wrist <- svm(activity ~ ., data = trainWrist[,-1], 
+                       gamma=tuned.wrist$best.parameters[1,1], 
+                       cost=tuned.wrist$best.parameters[1,2], 
+                       probability = TRUE)
+
+# make prediction (ignoring timestamp and annotation)
+svm.pred.wrist <- predict(svm.model.wrist, testWrist[,-c(1,2)], probability = TRUE)
+
+# calculate Stream Quality
+svm_SQ = rep(NA, length(svm.pred.wrist))
+for (i in 1:length(svm.pred.wrist)) {
+  svm_SQ[i] = attributes(svm.pred.wrist)$prob[i,as.character(svm.pred.wrist[i])]
 }
+testWristSQ = mean(svm_SQ)
+
+# Hip stream
+trainHip$activity = factor(trainHip$activity)
+
+# find best cost and gamma parameters for SVM
+tuned.hip <- tune.svm(activity ~ ., data = trainHip[,-1],
+                      gamma = 10^(-6:-1), cost = 10^(-1:1)) 
+
+# train model (ignoring timestamp)
+svm.model.hip <- svm(activity ~ ., data = trainHip[,-1], 
+                     gamma=tuned.hip$best.parameters[1,1], 
+                     cost=tuned.hip$best.parameters[1,2], 
+                     probability = TRUE)
+
+# make prediction (ignoring timestamp and annotation)
+svm.pred.hip <- predict(svm.model.hip, testHip[,-c(1,2)], probability = TRUE)
+
+# calculate Stream Quality
+svm_SQ = rep(NA, length(svm.pred.hip))
+for (i in 1:length(svm.pred.hip)) {
+  svm_SQ[i] = attributes(svm.pred.hip)$prob[i,as.character(svm.pred.hip[i])]
+}
+testHipSQ = mean(svm_SQ)
+
+# compare SQ and pick best stream
+# make assumption that hip has better SQ
+hipBetter = TRUE
+# placeholder for for final predictions
+pred = NA
+if (testHipSQ < testWristSQ) { 
+  hipBetter = FALSE
+  pred <- as.numeric(svm.pred.wrist)
+  cat("Wrist stream performs better; SQ: ", testWristSQ, "\n")
+  log = paste(wristDataPath, "wrist", tuned.wrist$best.parameters[1,2], 
+              tuned.wrist$best.parameters[1,1], sep=",")
+  write(log, file=logPath, append=TRUE)
+} else {
+  pred <- as.numeric(svm.pred.hip)
+  cat("Hip stream performs better; SQ: ", testHipSQ, "\n")
+  log = paste(wristDataPath, "wrist", tuned.hip$best.parameters[1,2], 
+              tuned.hip$best.parameters[1,1], sep=",")
+  write(log, file=logPath, append=TRUE)
+}
+
+# create output dir
+dir.create(file.path(path, "SVM"), showWarnings = FALSE)
+
+# save the graph
+if (hipBetter) {
+  pdf(paste(path,"/SVM/", file_path_sans_ext(hipDataPath),"_class_features",
+            (sum(filterTest)-2),".pdf",sep=""))
+} else {
+  pdf(paste(path,"/SVM/", file_path_sans_ext(wristDataPath),"_class_features",
+            (sum(filterTest)-2),".pdf",sep=""))
+}
+# produce plots of given data with the best prediction
+par(mfrow=c(2,1))
+plot(testWrist$statSummary, xlab = "Time", ylab="Intensity", 
+     type="l", main="Wrist data", ylim=c(0.07,max(testWrist$statSummary)))
+points(x=1:length(pred),y=rep(0.07,length(pred)),col=pred, pch=16)
+legend("topright", c("Rest", "Light", "High"), cex=1, fill=1:3)
+plot(testHip$statSummary, xlab = "Time", ylab="Intensity", 
+     type="l", main="Hip data", ylim=c(0.07,max(testHip$statSummary)))
+points(x=1:length(pred),y=rep(0.07,length(pred)),col=pred, pch=16)
+legend("topright", c("Rest", "Light", "High"), cex=1, fill=1:3)
+dev.off()
+
+# save the result
+# take away one from prediction as it counts as 1,2,3 isntead of 0,1,2 - see 'pred <- as.numeric(fit.knn)'
+outputWrist = data.frame(timestamp=testWrist$timestamp, 
+                         activity=testWrist$activity, prediction=pred-1)
+outputHip = data.frame(timestamp=testHip$timestamp, 
+                       activity=testHip$activity, prediction=pred-1)
+
+write.csv(outputWrist, 
+          paste(path,"/SVM/",file_path_sans_ext(wristDataPath),"_",sum(filterTest),"_prediction.csv",sep=""), 
+          row.names=FALSE)
+
+write.csv(outputHip, 
+          paste(path,"/SVM/",file_path_sans_ext(hipDataPath),"_",sum(filterTest),"prediction.csv",sep=""), 
+          row.names=FALSE)
